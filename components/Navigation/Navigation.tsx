366c4b88ce0086f3ac9ff1e0c315af4609cefc53
Nicolás Londoño <nicolas.londono@veevart.com>
2023-07-09 21:02:28 -0500

    Add: init project

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+const routes = [
+  { label: "Home", route: "/" },
+  { label: "About", route: "/about" },
+];
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        {routes.map((route) => {
+          return (
+            <li key={route.route}>
+              <Link href={route.route}> {route.label}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
